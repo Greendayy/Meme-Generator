71adb2c143850bff3181be2383debcf293d83e61
greenday <974023720@qq.com>
2022-08-27 22:22:13 +0800

    first commit

---
+++ Compenents/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar(){
+    <section className="navBar">
+<img src="troll-face.png" className="nav-img"/>
+<h2 className="nav-title">Meme Generator</h2>
+<h4 className="nav-text">React Course - Project 3</h4>
+    </section>
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Meme-generator-clone
+
+This is a practise.
+
+Figma:https://www.figma.com/file/MoLwFPHNHJVrzdFurxHzNV/Meme-Generator?node-id=0%3A1
+
+Scrimba:https://scrimba.com/learn/learnreact/using-an-async-function-inside-useeffect-clone-cw73rRud
+
+[![結果圖](WechatIMG23.png/)]
